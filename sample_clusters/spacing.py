24a0f7a743602cccd8e76b5b0cbb921fee7d5cfd
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-03-30 12:27:40 -0400

    Update spacing.py

--- sample_clusters/spacing.py
+++ sample_clusters/spacing.py
@@ -4,14 +4,16 @@ from sim_modules_vector import *
 from modules import *
 import numpy as np
 
-data = read_cpptraj(950)
+break = 950
+# Combine all the files into one files,
+# Normalize all data using the min-max normalization,
+# and getting the min and max for the whole dataset.
+
+data = read_cpptraj(break)
 norm = Normalize(data=data)
 normed_data = norm.get_normed_data()
 min, max = norm.get_min_max()
 np.savetxt("normed_data.txt", normed_data)
-#with open("minmax.txt", "w") as f:
-#	f.write(f"min is {min}\n")
-#	f.write(f"max is {max}")
 
-data = read_cpptraj(950, min=min, max=max, normalize=True)
-
+# Normalize each file using the min and max determined from above. 
+data = read_cpptraj(break, min=min, max=max, normalize=True)
