a2181eba043e41c6516d91828ed2a4953baae084
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-04-29 18:16:19 -0400

    updated

--- modules/rep_frames.py
+++ modules/rep_frames.py
@@ -1,4 +1,4 @@
-"""This script aims to find the representative frame for each method below. """
+"""This script aims to find the representative frame for each method below."""
 from modules.esim import calculate_medoid
 from modules.sim_calc import trim_outliers
 import numpy as np
@@ -19,16 +19,25 @@ def calculate_max_key(dict):
     max_key = int(re.findall(r'\d+', max_key)[0])
     return max_key
 
-def gen_method_max(weighted=True, trim=0.1, n_ary="RR", weight='nw', output_name="rep"):
-    if weighted is True:
+def gen_method_max(weighted_by_frames=True, trim_frac=0.1, n_ary="RR", weight='nw', output_name="rep"):
+    """Generate the representative frame for each method.
+
+    Args:
+        weighted_by_frames (bool, optional): Similarity is weighted by frames. Defaults to True.
+        trim_frac (float, optional): . Defaults to 0.1.
+        n_ary (str, optional): _description_. Defaults to "RR".
+        weight (str, optional): _description_. Defaults to 'nw'.
+        output_name (str, optional): _description_. Defaults to "rep".
+    """
+    if weighted_by_frames is True:
         w = "w_"
-    elif weighted is False:
+    elif weighted_by_frames is False:
         w = ""
-    if trim is not None:
-        t = f"_t{int(float(trim) * 100)}"
-    elif trim is None:
+    if trim_frac:
+        t = f"_t{int(float(trim_frac) * 100)}"
+    elif not trim_frac:
         t= ""
-    with open(f"{w}{output_name}_{n_ary}{t}.txt","w") as output:
+    with open(f"w/{w}{output_name}_{n_ary}{t}.txt","w") as output:
         output.write("# Frame number with max values by method: medoid_all, medoid_c0, medoid_c0(trimmed), pairwise, union, medoid, outlier\n")
         
         # medoid_all
@@ -40,31 +49,31 @@ def gen_method_max(weighted=True, trim=0.1, n_ary="RR", weight='nw', output_name
         output.write(f"{calculate_medoid(c0, n_ary=n_ary, weight=weight)}, ")
         
         # medoid_c0 (trimmed)
-        if trim is None:
+        if not trim_frac:
             output.write(f"{calculate_medoid(c0, n_ary=n_ary, weight=weight)}, ")
-        if trim is not None:
-            trim_c0 = trim_outliers(c0, trim_frac=trim, n_ary=n_ary, weight=weight)
+        if trim_frac:
+            trim_c0 = trim_outliers(c0, trim_frac=trim_frac, n_ary=n_ary, weight=weight)
             index = calculate_medoid(trim_c0)
             search = trim_c0[index]
             new_index = np.where((c0 == search).all(axis=1))[0]
             output.write(f"{new_index[0]}, ")
         
         # pairwise
-        with open(f"{w}pairwise_{n_ary}{t}.txt", "r") as file:
+        with open(f"w/{w}pairwise_{n_ary}{t}.txt", "r") as file:
             pairwise = json.load(file)
         output.write(f"{calculate_max_key(pairwise)}, ")
 
         # union
-        with open(f"{w}union_{n_ary}{t}.txt", "r") as file:
+        with open(f"w/{w}union_{n_ary}{t}.txt", "r") as file:
             union = json.load(file)
         output.write(f"{calculate_max_key(union)}, ")
 
         # medoid
-        with open(f"{w}medoid_{n_ary}{t}.txt", "r") as file:
+        with open(f"w/{w}medoid_{n_ary}{t}.txt", "r") as file:
             medoid = json.load(file)
         output.write(f"{calculate_max_key(medoid)}, ")
         
         # outlier
-        with open(f"{w}outlier_{n_ary}{t}.txt", "r") as file:
+        with open(f"w/{w}outlier_{n_ary}{t}.txt", "r") as file:
             outlier = json.load(file)
         output.write(f"{calculate_max_key(outlier)}")
--- rep_frames.py
+++ rep_frames.py
@@ -8,7 +8,7 @@ Trim 10% outliers
 >>> python rep.py -t 0.1 -i RR
 """
 parser = argparse.ArgumentParser(description='Generate method max with optional trim and n_ary')
-parser.add_argument('-t', '--trim', type=float, default=None,
+parser.add_argument('-t', '--trim_frac', type=float, default=None,
                     help='Trim parameter for gen_method_max method')
 parser.add_argument('-i', '--index', type=str, default='RR',
                     help='n_ary parameter for gen_method_max method')
@@ -17,4 +17,4 @@ args = parser.parse_args()
 if args.trim is None:
     mod.gen_method_max(n_ary=args.index)
 else:
-    mod.gen_method_max(trim=args.trim, n_ary=args.index)
+    mod.gen_method_max(trim_frac=args.trim_frac, n_ary=args.index)
