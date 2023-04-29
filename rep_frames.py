fa80800dac9ba3f2db55a395271b5ee162577d4a
Lexin Chen <110304515+lexin-chen@users.noreply.github.com>
2023-04-29 18:26:59 -0400

    updated examples

--- rep_frames.py
+++ rep_frames.py
@@ -2,10 +2,14 @@ import argparse
 import modules as mod
 
 """Call the `gen_method_max` with the parsed trim and index argument
-No trim
->>> python rep.py -i SM
-Trim 10% outliers
->>> python rep.py -t 0.1 -i RR
+Example usage:
+----------------
+No trim, RR index
+>>> python scripts/rep_frames.py
+10% trim, RR index
+>>> python scripts/rep_frames.py -t 0.1
+20% trim, SM index
+>>> python scripts/rep_frames.py -t 0.2 -i SM
 """
 parser = argparse.ArgumentParser(description='Generate method max with optional trim and n_ary')
 parser.add_argument('-t', '--trim_frac', type=float, default=None,
@@ -14,7 +18,4 @@ parser.add_argument('-i', '--index', type=str, default='RR',
                     help='n_ary parameter for gen_method_max method')
 args = parser.parse_args()
 
-if args.trim is None:
-    mod.gen_method_max(n_ary=args.index)
-else:
-    mod.gen_method_max(trim_frac=args.trim_frac, n_ary=args.index)
+mod.gen_method_max(trim_frac=args.trim_frac, n_ary=args.index)
