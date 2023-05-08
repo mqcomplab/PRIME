3adf0006797a095c74f59821c06712bc8a62e0d1
lexin-chen <lxchen017@gmail.com>
2023-05-08 12:25:11 -0400

    updated

--- modules/preprocess.py
+++ modules/preprocess.py
@@ -26,12 +26,12 @@ class Normalize:
     max (float): The maximum value of the input data.
     """    
     def __init__(self, file_path=None, data=None, custom_min=None, custom_max=None):
-        if file_path is not None:
+        if file_path:
             self.file_path = file_path
             self.data = np.genfromtxt(self.file_path)
-        elif data is not None:
+        elif data:
             self.data = data
-        if custom_min is not None and custom_max is not None:
+        if custom_min and custom_max:
             self.min = custom_min
             self.max = custom_max
         else:
@@ -40,14 +40,18 @@ class Normalize:
         self.normed_data = (self.data - self.min) / (self.max - self.min)
         self.esim_norm = 1 - np.abs(self.normed_data - np.mean(self.normed_data, axis=0))
         self.c_total = np.sum(1 - np.abs(self.normed_data - np.mean(self.normed_data, axis=0)), axis=0)
-    def get_c_total(self):
-        return self.c_total
-    def get_normed_data(self):
-        return self.normed_data
+    
     def get_min_max(self):
         return self.min, self.max
+
+    def get_normed_data(self):
+        return self.normed_data
+    
     def get_esim_norm(self):
         return self.esim_norm
+    
+    def get_c_total(self):
+        return self.c_total
 
 def read_cpptraj(break_line, min=None, max=None, normalize=False):
     """Read CPPTRAJ files to convert to numpy ndarray formatting and normalize the data.
@@ -76,13 +80,13 @@ def read_cpptraj(break_line, min=None, max=None, normalize=False):
         str_frames = [list(chain.from_iterable(chunk)) for chunk in chunks]
         str_frames = [' '.join(frame) for frame in str_frames]
         frames = np.array([np.fromstring(frame, dtype='float32', sep=' ') for frame in str_frames])
-        if normalize is True:
+        if normalize:
             norm = Normalize(data=frames, custom_min=min, custom_max=max)
             normed_frame = norm.get_normed_data()
             np.savetxt(f"normed_{file}", normed_frame)
         else:
             frames_list.append(frames)
         count_frames.append(len(frames))
-    if normalize is False:
+    if not normalize:
         data = np.concatenate(frames_list, axis=0)
         return data
